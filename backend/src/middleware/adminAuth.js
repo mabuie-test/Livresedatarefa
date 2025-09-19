3b0f6e0c724eb152df8ee7b11975f96948ae5327
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-19 17:03:52 +0200

    Create adminAuth.js

---
+++ backend/src/middleware/adminAuth.js
@@ -0,0 +1,5 @@
+module.exports = function(req, res, next) {
+  if (!req.user) return res.status(401).json({ error: 'Autenticacao necessaria' });
+  if (req.user.role !== 'admin') return res.status(403).json({ error: 'Acesso negado' });
+  next();
+};
