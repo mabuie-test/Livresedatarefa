2d66d5c38a0eb640ad79952871acf0272daa3506
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-25 21:46:51 +0200

    Create auth.js

---
+++ Servfront/src/lib/auth.js
@@ -0,0 +1,3 @@
+export function saveToken(token){ localStorage.setItem('token', token); }
+export function getToken(){ return localStorage.getItem('token'); }
+export function logout(){ localStorage.removeItem('token'); window.location.href = '/login'; }
