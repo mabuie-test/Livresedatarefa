93677dab3f6a47cf69567e16b80cd2f246af4a15
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-26 07:54:15 +0200

    Create Home.jsx

---
+++ Front/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import OrderWizard from '../components/OrderWizard.jsx';
+import { Box } from '@chakra-ui/react';
+
+export default function Home(){
+  return (
+    <Box>
+      <OrderWizard />
+    </Box>
+  );
+}
