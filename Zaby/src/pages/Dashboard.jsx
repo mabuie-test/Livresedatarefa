946011758aa0bac09d7e1c5fcf9197d62cb97f17
JorgeMabuie <jorgemaabuie@gmail.com>
2025-09-26 20:16:06 +0200

    Update Dashboard.jsx

--- Zaby/src/pages/Dashboard.jsx
+++ Zaby/src/pages/Dashboard.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import api from '../lib/api.js';
+import { useNavigate } from 'react-router-dom';
 
 export default function Dashboard(){
   const [orders,setOrders]=useState([]);
+  const navigate = useNavigate();
+
   useEffect(()=>{ async function load(){ try{ const res = await api.get('/api/orders'); setOrders(res.data.orders||[]); }catch(e){ console.error(e);} } load(); },[]);
+
   return (
     <div className="container">
       <h3>Meus Pedidos</h3>
@@ -16,7 +20,7 @@ export default function Dashboard(){
               <div className="small">{o.serviceType} • {o.pages} pág • {o.totalPriceMZN} MZN</div>
             </div>
             <div>
-              <button className="btn" onClick={()=>window.location.href=`/orders/${o._id}`}>Ver</button>
+              <button className="btn" onClick={()=>navigate(`/orders/${o._id}`)}>Ver</button>
             </div>
           </div>
         ))}
