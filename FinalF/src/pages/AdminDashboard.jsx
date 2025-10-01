cdc8ace66c765854450ffe996d54b1ce15cfe433
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-01 11:06:28 +0200

    Create AdminDashboard.jsx

---
+++ FinalF/src/pages/AdminDashboard.jsx
@@ -0,0 +1,2 @@
+import React from 'react';
+export default function AdminDashboard(){ return <div className="card"><h2>Admin</h2><div>Aqui podes listar e gerir encomendas (implementar conforme necessário).</div></div>; }
