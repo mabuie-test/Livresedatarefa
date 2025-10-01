0dbe52ff5a8099e80dfc455e25fe9e7ca50fa5b0
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-01 10:22:32 +0200

    Create server.js

---
+++ Final/src/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const http = require('http');
+const app = require('./app');
+
+const PORT = process.env.PORT || 4000;
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Backend listening on port ${PORT}`);
+});
