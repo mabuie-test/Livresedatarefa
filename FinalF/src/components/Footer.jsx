35ec36cacdc6b1f6e518e26e877c84cac9c61a6e
JorgeMabuie <jorgemaabuie@gmail.com>
2025-10-01 10:48:40 +0200

    Create Footer.jsx

---
+++ FinalF/src/components/Footer.jsx
@@ -0,0 +1,2 @@
+import React from 'react';
+export default function Footer(){ return <footer className="footer">© {new Date().getFullYear()} Livresedatarefa — Todos os direitos ao JM_tech.</footer>; }
